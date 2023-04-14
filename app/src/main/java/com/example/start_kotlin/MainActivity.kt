6931860fa26ade7f6fc18336b043d3449a3de4b1
기남석 <kee0304@naver.com>
2023-04-14 17:24:19 +0900

    kotlin 3rd

--- app/src/main/java/com/example/start_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/start_kotlin/MainActivity.kt
@@ -7,18 +7,20 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val resultImage : ImageView = findViewById(R.id.dice_image)
+        val resultImage1 : ImageView = findViewById(R.id.dice_image1)
+        val resultImage2 : ImageView = findViewById(R.id.dice_image2)
         val rollButton : Button = findViewById(R.id.roll_button)
-        val resultText : TextView = findViewById(R.id.result_text)
-        rollButton.setOnClickListener {
-            val randomInt = (1..6).random()
-            val drawableResource = when (randomInt) {
+        rollButton.setOnClickListener {view ->
+            Snackbar.make(view, "onClicked", Snackbar.LENGTH_LONG).setAction("Action",null).show()
+            val randomInt1 = (1..6).random()
+            val drawableResource1 = when (randomInt1) {
                 1 -> R.drawable.dice_1
                 2 -> R.drawable.dice_2
                 3 -> R.drawable.dice_3
@@ -26,8 +28,17 @@ class MainActivity : AppCompatActivity() {
                 5 -> R.drawable.dice_5
                 else -> R.drawable.dice_6
             }
-            resultText.text = randomInt.toString()
-            resultImage.setImageResource(drawableResource)
+            val randomInt2 = (1..6).random()
+            val drawableResource2 = when (randomInt2) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            resultImage1.setImageResource(drawableResource1)
+            resultImage2.setImageResource(drawableResource2)
         }
     }
 
